6b4fe96a24997a4bb849c88c641c29f787a31d4d
pelidigatto <florianthoenelt@outlook.de>
2024-12-15 12:53:07 +0100

    Refactor SkillBubbles component to use TypeScript interface for props and improve skill handling

--- app/components/SkillBubbles/skillbubbles.tsx
+++ app/components/SkillBubbles/skillbubbles.tsx
@@ -1,16 +1,18 @@
-const SkillBubbles = (props: { skills: any; }) => { 
-    var skills = props.skills;
-        skills = skills.split(",");
-    
+interface SkillBubblesProps {
+    skills: string;
+}
+
+const SkillBubbles = (props: SkillBubblesProps) => { 
+    const skills = props.skills.split(",");
+
     return(
         <div className="flex flex-row justify-center flex-wrap">
-            {skills.map((skill: any, index: number) => (
+            {skills.map((skill: string, index: number) => (
                 <div key={index} className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">
                     <p>{skill}</p>
                 </div>
             ))}
         </div>
-
     )
 }
 
