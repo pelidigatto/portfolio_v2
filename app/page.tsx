cf7278e634e56329f010264ca2dc67d739fe8f63
pelidigatto <florianthoenelt@outlook.de>
2024-12-15 07:40:16 +0100

    Refactor skill section styles for improved font consistency and layout

--- app/page.tsx
+++ app/page.tsx
@@ -63,27 +63,26 @@ export default function Home() {
             </div>
             <div className="col-span-12 md:col-span-6 ">
               <div className="flex flex-row justify-center flex-wrap">
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">HTML</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">CSS</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">JavaScript</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">PHP</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Next.JS</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">React</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Node.JS</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Vue.JS</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">TailwindCSS</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">ColdFuison</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Bootstrap</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">GIT</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">SEO</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Bash</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Shell</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Linux</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Wordpress</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">RestAPI</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">MySQL</div>
-                <div className="m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">C#</div>
-
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">HTML</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">CSS</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">JavaScript</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">PHP</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Next.JS</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">React</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Node.JS</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Vue.JS</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">TailwindCSS</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">ColdFuison</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Bootstrap</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">GIT</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">SEO</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Bash</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Shell</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Linux</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">Wordpress</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">RestAPI</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">MySQL</div>
+                <div className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">C#</div>
               </div>
             </div>
           </div>
@@ -91,9 +90,11 @@ export default function Home() {
 
         <div className="flex justify-center items-center my-10">
           <div className="container mx-auto w-full h-full">
+            <div className="col-span-12 my-10">
               <div className="text-2xl md:text-4xl font-thin text-center">
                 <h2>Mein Werdegang</h2>
               </div>
+            </div>
             <div className="relative wrap p-2 sm:p-10 h-full">
               <div className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border-0 md:border timeline" /* style={{ left: '50%' }} */></div>
               
