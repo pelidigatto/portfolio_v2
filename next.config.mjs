b3888cee2ab3525c9e9850a28b3b961b04acba80
pelidigatto <florianthoenelt@outlook.de>
2025-01-23 04:51:13 +0100

    Refactor Next.js configuration: consolidate redirects and remove deprecated config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,31 +2,26 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  trailingSlash: true, // Beibehaltung der aktuellen Einstellung
-
-  // Hinzufügen der Redirects
+  trailingSlash: true,
   async redirects() {
     return [
+      
       {
         source: '/:path*',
         has: [
           {
             type: 'host',
-            value: 'www.:domain', // Prüft auf www-Domain
+            value: 'www.:domain', // Universelle Weiterleitung
           },
         ],
-        destination: 'https://:domain/:path*', // Basis-URL ohne www
-        permanent: true, // Permanente Weiterleitung (301)
+        destination: 'https://:domain/:path*',
+        permanent: true,
       },
     ];
   },
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
+const withMDX = createMDX();
 
-// Merge MDX config with Next.js config
 export default withMDX(nextConfig);
