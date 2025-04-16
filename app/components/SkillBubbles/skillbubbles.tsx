0627bb9c57bdc4aee28f2c3dff08931c194ca5dd
pelidigatto <florianthoenelt@outlook.de>
2025-04-16 05:50:06 +0200

    [C] Remove shadow on skill bubbles

--- app/components/SkillBubbles/skillbubbles.tsx
+++ app/components/SkillBubbles/skillbubbles.tsx
@@ -1,20 +1,22 @@
 interface SkillBubblesProps {
-    skills: string;
+  skills: string;
 }
 
-const SkillBubbles = (props: SkillBubblesProps) => { 
-    const skills = props.skills.split(",");
+const SkillBubbles = (props: SkillBubblesProps) => {
+  const skills = props.skills.split(",");
 
-    return(
-        <div className="flex flex-row justify-center flex-wrap">
-            {skills.map((skill: string, index: number) => (
-                <div key={index} className="font-thin m-3 border p-3 bg-slate-100 rounded-lg shadow text-center">
-                    <p>{skill}</p>
-                </div>
-            ))}
+  return (
+    <div className="flex flex-row justify-center flex-wrap">
+      {skills.map((skill: string, index: number) => (
+        <div
+          key={index}
+          className="font-thin m-3 border p-3 bg-slate-100 rounded-lg text-center"
+        >
+          <p>{skill}</p>
         </div>
-    )
-}
-
+      ))}
+    </div>
+  );
+};
 
 export default SkillBubbles;
