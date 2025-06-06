9b6c55311f814cacc77ea0dd24acdf61a57509db
pelidigatto <florianthoenelt@outlook.de>
2025-06-06 06:51:52 +0200

    [F] New Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,21 +33,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de">
-      <head>
-        <title>Florian Thönelt | Dev</title>
-        <script
+    <head>
+      <title>Florian Thönelt | Dev</title>
+      <script
           async
           type="text/javascript"
           src="//cdn.cookie-script.com/s/102ae36c00073d80e7b5b6dd4575b25b.js"
-        ></script>
-        <script defer data-domain="thoenelt.dev" src="https://plausible.thnlt.de/js/script.js"></script>
-      </head>
-      <body className={`antialiased`}>
-        <Header />
-        <div className="relative mx-auto lg:max-w-7xl px-2 py-1">
-          {children}
-        </div>
-      </body>
+      ></script>
+      <script defer data-domain="thoenelt.dev" src="https://plausible.thnlt.de/js/script.js"></script>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+    </head>
+    <body className={`antialiased`}>
+    <Header/>
+    <div className="relative mx-auto lg:max-w-7xl px-2 py-1">
+      {children}
+    </div>
+    </body>
     </html>
   );
 }
